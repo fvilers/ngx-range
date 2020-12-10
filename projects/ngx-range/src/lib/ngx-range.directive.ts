f65a814495c7a8def668ddefd829d3aa1aa2f152
Fabian Vilers <fabian.vilers@dev-one.com>
2020-12-10 07:11:48 +0100

    Add the by property for step value

--- projects/ngx-range/src/lib/ngx-range.directive.ts
+++ projects/ngx-range/src/lib/ngx-range.directive.ts
@@ -17,16 +17,23 @@ export class NgxRangeDirective implements OnChanges {
   @Input()
   ngxRangeTo: number = 0;
 
+  @Input()
+  ngxRangeBy: number = 1;
+
   constructor(
     private templateRef: TemplateRef<any>,
     private viewContainer: ViewContainerRef
   ) {}
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes.ngxRangeFrom || changes.ngxRangeTo) {
+    if (changes.ngxRangeFrom || changes.ngxRangeTo || changes.ngxRangeBy) {
       this.viewContainer.clear();
 
-      for (let i = this.ngxRangeFrom; i < this.ngxRangeTo; i++) {
+      for (
+        let i = this.ngxRangeFrom;
+        i < this.ngxRangeTo;
+        i += this.ngxRangeBy
+      ) {
         this.viewContainer.createEmbeddedView(this.templateRef, {
           $implicit: i,
         });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  repeat = 3;
+  repeat = 5;
 
   more() {
     this.repeat++;
