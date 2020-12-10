e772bfaa8a4d6858cb115346e2bba9ed72866e1b
Fabian Vilers <fabian.vilers@dev-one.com>
2020-12-10 06:09:00 +0100

    Remove default content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'ngx-range-demo';
-}
+export class AppComponent {}
